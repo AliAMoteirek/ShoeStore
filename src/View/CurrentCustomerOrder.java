fbb3cedc57b8f8b166b08fa397e8b98ae47cfb36
Ali Moteirek <mtairek20@gmail.com>
2022-02-23 10:35:21 +0100

    Added CurrentCustomerOrder as a Record

---
+++ src/View/CurrentCustomerOrder.java
@@ -0,0 +1,18 @@
+package View;
+
+import java.sql.Timestamp;
+
+public record CurrentCustomerOrder(int id, String invoiceNumber, Timestamp dateTime) {
+
+    public int getId() {
+        return id;
+    }
+
+    public String getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public Timestamp getDateTime() {
+        return dateTime;
+    }
+}
